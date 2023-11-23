61838cbd890c9610865d09f7394d289b80144d09
KARBOU Aristide <aristidetiger12@gmail.com>
2023-11-23 02:33:19 +0000

    render blogcard

--- src/components/Blogs.tsx
+++ src/components/Blogs.tsx
@@ -1,12 +1,12 @@
 import React from "react";
+import BlogCard from "./BlogCard";
 
 function Blogs() {
   return (
     <>
-      <div>Blogs</div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
         <div>
-            
+            <BlogCard/>
         </div>
       </div>
     </>
