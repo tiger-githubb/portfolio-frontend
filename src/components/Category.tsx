5ca8d6807ea4fce44bc1e9eb95e6d82f7dde6c3e
KARBOU Aristide <aristidetiger12@gmail.com>
2023-11-23 01:16:06 +0000

    layout for category

---
+++ src/components/Category.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Category = () => {
+  return (
+    <div>
+      <div className=" bg-slate-400 p-4 rounded-lg shadow-md cursor-pointer ">
+        Titre
+      </div>
+    </div>
+  );
+};
+
+export default Category;
