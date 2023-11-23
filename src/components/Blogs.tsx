d81d91353d1ba9d1c2497fb937e82e8f3f0b4959
KARBOU Aristide <aristidetiger12@gmail.com>
2023-11-23 01:24:33 +0000

    add of the layout

--- src/components/Blogs.tsx
+++ src/components/Blogs.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 function Blogs() {
   return (
-    <div>Blogs</div>
-  )
+    <>
+      <div>Blogs</div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+        <div>
+            
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Blogs
+export default Blogs;
