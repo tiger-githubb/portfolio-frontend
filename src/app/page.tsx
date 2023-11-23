e800bbfbdf6b601820e232dfc531e8bec2976cfa
KARBOU Aristide <aristidetiger12@gmail.com>
2023-11-23 01:16:52 +0000

    render category in tha page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Categories } from '@/components/Categories'
 import Image from 'next/image'
 
 export default function Home() {
   return (
    <>
    <h2>Home Page</h2>
+   <Categories/>
    </>
   )
 }
