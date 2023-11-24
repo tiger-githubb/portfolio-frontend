83e1b900618f03b17a026cbfdc1121fff4eb4e06
KARBOU Aristide <aristidetiger12@gmail.com>
2023-11-24 21:01:22 +0000

    get data for sigle blog

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -1,25 +1,38 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-function BlogCard() {
+function BlogCard({ blog }: any) {
+  const tuncateBlogDescription =
+    blog?.attributes.Description.length > 80
+      ? blog?.attributes.Description.substring(0, 80) + "..."
+      : blog?.attributes.Description;
+
+  const imageUrl = ""+blog?.attributes.Image?.data?.attributes?.url;
+
   return (
     <>
-    <div className='roundec-lg shadow-md p-4 mb-4 overflow-hidden border-gray-600 cursor-pointer'>
+      <div className="roundec-lg shadow-md p-4 mb-4 overflow-hidden border-gray-600 cursor-pointer">
         <Link href={"/blog/23"}>
-            <div>
-            <Image src={""} alt='' layout='fill' objectFit='cover'className='rounded-t-lg'/>
-            </div>
-            <div className='p-2'> 
-                <h2 className="text-xl font-semibold mb-2 overflow-ellipsis">Blog Title</h2>
-                <p className="text-grey-600">
-                    Description of the article
-                </p>
-            </div>
+          <div>
+            <Image
+              src={""}
+              alt=""
+              layout="fill"
+              objectFit="cover"
+              className="rounded-t-lg"
+            />
+          </div>
+          <div className="p-2">
+            <h2 className="text-xl font-semibold mb-2 overflow-ellipsis">
+              {blog?.attributes.Title}
+            </h2>
+            <p className="text-grey-600">{tuncateBlogDescription}</p>
+          </div>
         </Link>
-    </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default BlogCard
+export default BlogCard;
