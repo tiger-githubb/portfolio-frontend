8ede106b7b22ac88742af9d0e4e75e1b22427d7b
KARBOU Aristide <aristidetiger12@gmail.com>
2023-11-24 04:01:15 +0000

    pasing data with props

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 
-const Category = () => {
+const Category = ({ cat }: any) => {
   return (
-    <div>
-      <div className=" bg-slate-400 p-4 rounded-lg shadow-md cursor-pointer ">
-        Titre
-      </div>
+    <div className=" bg-slate-400 p-4 rounded-lg shadow-md cursor-pointer ">
+      {cat.attributes.Title}
     </div>
   );
 };
