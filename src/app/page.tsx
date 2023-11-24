e10369435bcc5b4c07f8cc9fd801e45a0da06fef
KARBOU Aristide <aristidetiger12@gmail.com>
2023-11-24 04:00:05 +0000

    fetchcategories data passing with props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,29 @@
-import Blogs from '@/components/Blogs'
-import { Categories } from '@/components/Categories'
-import Image from 'next/image'
+import Blogs from "@/components/Blogs";
+import { Categories } from "@/components/Categories";
 
-export default function Home() {
+async function fetchcategories() {
+  const options = {
+    Headers: {
+      Autorization: `Bearer ${process.env.STRAPI_API_TOKEN}`,
+    },
+  };
+
+  try {
+    const res = await fetch("http://127.0.0.1:1337/api/categories");
+    const response = await res.json();
+    console.log(response);
+        
+    return response;
+  } catch {}
+}
+
+export default async function Home() {
+  const categories = await fetchcategories();
   return (
-   <>
-   <h2>Home Page</h2>
-   <Categories/>
-   <Blogs/>
-   </>
-  )
+    <>
+      <h2>Home Page</h2>
+      <Categories categories={categories} />
+      <Blogs />
+    </>
+  );
 }
