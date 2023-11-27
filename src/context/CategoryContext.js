dd25c5d10c7c5269e8a3c1db1089975f79f3797a
KARBOU Aristide <aristidetiger12@gmail.com>
2023-11-27 14:09:48 +0000

    provider function

--- src/context/CategoryContext.js
+++ src/context/CategoryContext.js
@@ -2,3 +2,17 @@
 
 import { createContext, useState } from "react";
 
+
+// Provider component that wraps the app and makes category available to any child component that calls it.
+export const CategoryProvider = ({ children }) => {
+    const [category, setCategory] = useState("");
+    const changeCategory = (cat) => {
+        setCategory(cat);
+    }
+
+    return (
+        <CategoryContext.Provider value={{ category, changeCategory }}>
+            {children}
+        </CategoryContext.Provider>
+    )
+}
