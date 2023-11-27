e24317d08549c7894af7248204d51f483dd83343
KARBOU Aristide <aristidetiger12@gmail.com>
2023-11-27 14:56:42 +0000

    filteredBlogs function

--- src/components/Blogs.tsx
+++ src/components/Blogs.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import BlogCard from "./BlogCard";
+import { CategoryContext } from "@/context/CategoryContext";
 
 const Blogs = ({ blogs }: any) => {
+  const { category } = useContext(CategoryContext);
+
+  const filteredBlogs = blogs?.data.filter((blog: any) => {
+    return blog.attributes.categories.data.some(
+      (cat:any) =>cat.attributes.Title === category
+    )
+  })
   return (
     <>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
