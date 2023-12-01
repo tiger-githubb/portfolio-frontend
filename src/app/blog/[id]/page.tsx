9c94bb6048859682028310404a9f5766ded9b253
KARBOU Aristide <aristidetiger12@gmail.com>
2023-12-01 22:06:12 +0000

    renderin and reorganisation

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -43,7 +43,14 @@ export default async function page({ params }: any) {
       <div className="max-w-3xl mx-auto p-4">
         <Link href={"/"}>{"Back"}</Link>
         <div className="relative w-full h-96 overflow-hidden rounded-lg mt-5">
-
+        <Image
+            src={imageUrl}
+            alt={""}
+            layout="fill"
+            priority
+            objectFit="cover"
+          />
+        </div>
 
       </div>
     </>
